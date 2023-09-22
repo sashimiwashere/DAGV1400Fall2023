faf0f3f06e48cb73ea661b92949fc69e222c957a
sashimiwashere <143565416+sashimiwashere@users.noreply.github.com>
2023-09-21 19:47:37 -0600

    Update Unit_1c_Challenge.cs

--- Unit_1c_Challenge.cs
+++ Unit_1c_Challenge.cs
@@ -8,29 +8,31 @@ public class Program_Challenge1
 
         int guess;
 
-        int num = 7;
+        Random random = new Random();
+
+        int num = random.Next(1, 11);
 
         int count = 0;
         do
         {
             guess = Convert.ToInt32(Console.ReadLine());
 
-            count++;
+            count++;  // everytime run this loop count will go up by 1
 
-            if (guess < num)
+            if (guess < num)  // if the guess is smaller than 7 show the text tells to guess bigger
             {
                 Console.WriteLine("Guess a little bit bigger");
             }
-            else if (guess > num)
+            else if (guess > num)  // if the guess is bigger than 7 show the text tells to guess smaller
             {
                 Console.WriteLine("Guess a little bit smaller");
             }
-            else
+            else  // if guess is not bigger or smaller than the actual number means he got it right
             {
-                Console.WriteLine("You got it!");
-                Console.WriteLine(("You got it at the ") + (count) + (" try"));
+                Console.WriteLine("You got it!");  
+                Console.WriteLine(("You got it at the ") + (count) + (" try"));  // shows the count nunber
             }
         }
-        while (guess != num);
+        while (guess != num);  // if the guess is not equal to the actual number run the loop again
     }
 }
