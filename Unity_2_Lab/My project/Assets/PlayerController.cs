d26652011e4b43461b48dd3211a04050ccdaa81b
sashimiwashere <s963215@gmail.com>
2023-10-04 07:32:28 -0600

    Enhancing the Player Controller (Unit_3_Challenge)

--- Unity_2_Lab/My project/Assets/PlayerController.cs
+++ Unity_2_Lab/My project/Assets/PlayerController.cs
@@ -7,9 +7,23 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed = 5;
 
-    public float gravity = -20f;
+    public float gravity = -30f;
 
-    public float jumpSpeed = 10;
+    public float jumpForce = 15;
+
+    public float sprintSpeed = 10;
+
+    public float crouchSpeed = 2;
+
+    public float standHeight = 2;
+
+    public float crouchHeight = 1;
+
+    public float rotationSpeed = 90;
+
+    private bool crouch;
+
+    private bool doubleJump;
 
     CharacterController characterController;
 
@@ -17,33 +31,56 @@ public class PlayerController : MonoBehaviour
 
     Vector3 turnVelocity;
 
-
     void Start()
     {
-        characterController = GetComponent<CharacterController>(); 
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         var hInput = Input.GetAxis("Horizontal");
 
-        var vInput = Input.GetAxis("Vertical"); 
+        var vInput = Input.GetAxis("Vertical");
 
-        if(characterController.isGrounded)
+        float mouseX = Input.GetAxis("Mouse X");
+
+        if(characterController.isGrounded || doubleJump)
         {
             moveVelocity.x = moveSpeed * hInput;
             moveVelocity.z = moveSpeed * vInput;
-
-            if(Input.GetButtonDown("Jump"))
+            turnVelocity = transform.up * rotationSpeed * mouseX;
+            
+            if(Input.GetButtonDown("Jump") && !crouch) //jump
             {
-                moveVelocity.y = jumpSpeed;
+                moveVelocity.y = jumpForce;
+                doubleJump = !doubleJump;//double jump 
+            }
+            if(Input.GetKey(KeyCode.LeftShift)) //sprint
+            {
+                moveVelocity.x = sprintSpeed * hInput;
+                moveVelocity.z = sprintSpeed * vInput;
+            }
+            if(Input.GetKey(KeyCode.LeftControl)) //crouch
+            {
+                moveVelocity.x = crouchSpeed * hInput;
+                moveVelocity.z = crouchSpeed * vInput;
+                characterController.height = crouchHeight;
+                crouch = true;
+            }
+            if(Input.GetKeyUp(KeyCode.LeftControl)) //uncrouching
+            {
+                characterController.height = standHeight;
+                crouch = false;
             }
         }
 
-        moveVelocity.y += gravity * Time.deltaTime;
+        moveVelocity.y += gravity * Time.deltaTime; //gravity
 
         characterController.Move(moveVelocity * Time.deltaTime);
 
-        
+        transform.Rotate(turnVelocity * Time.deltaTime);
+
     }
 }
